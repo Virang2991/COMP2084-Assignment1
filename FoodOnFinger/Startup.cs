41347355a260921d2dfb741c9ae434bfb7cc910a
Virang2991 <46640350+Virang2991@users.noreply.github.com>
2019-02-03 18:05:53 -0500

    Add project files.

---
+++ FoodOnFinger/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FoodOnFinger.Startup))]
+namespace FoodOnFinger
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
