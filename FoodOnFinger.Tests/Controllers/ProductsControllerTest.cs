abd8fcdef80863eff07a9931e63e5c4541aca990
Virang2991 <46640350+Virang2991@users.noreply.github.com>
2019-03-31 14:38:28 -0400

    Added ProductsControllerTest and Moq

---
+++ FoodOnFinger.Tests/Controllers/ProductsControllerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace FoodOnFinger.Tests.Controllers
+{
+    [TestClass]
+    public class ProductsControllerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
